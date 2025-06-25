a5bc62a7637c198c9a58ab9ba9e914aa70202016
Fahim Hossain <fahimhossain.hasib@gmail.com>
2025-06-25 00:50:15 -0400

    .

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yy%qjd&2y@mjp@03^^w@r^5d_na$5%wel-iohh+#zw-rpneqqk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
